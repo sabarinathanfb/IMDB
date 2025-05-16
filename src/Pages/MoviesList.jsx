03683ca054ff7b43914c461d6c91efd23c30aa9b
sabarinathanfb <77018757+sabarinathanfb@users.noreply.github.com>
2025-05-16 07:24:19 +0530

    Pagination List Changes Dynamically Depend on TotalPage

--- src/Pages/MoviesList.jsx
+++ src/Pages/MoviesList.jsx
@@ -5,6 +5,7 @@ import { Pagination } from '../component/Pagination'
 export const MoviesList = () => {
 
     const [movies,setMovies] = useState([])
+    const [totalPages,setTotalPages] = useState(0)
 
     const fetchMovies = (pageNo) =>{
         fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US&page=${pageNo}`).then((response) => {
@@ -14,6 +15,7 @@ export const MoviesList = () => {
             return response.json()
         }).then((data) => {
             setMovies(data.results)
+            setTotalPages(data.total_pages)
         }).catch((error) => {
             console.error("There was a problem with the fetch operation:", error);
         });
@@ -29,7 +31,7 @@ export const MoviesList = () => {
             <h1>Movie List</h1>
             <MovieList movies={movies}/>
             <div className='pagination-section'>
-                <Pagination onPageChange={fetchMovies}/>
+                <Pagination onPageChange={fetchMovies} totalPages={totalPages}/>
             </div>
         </div>
     )
--- src/component/Pagination.jsx
+++ src/component/Pagination.jsx
@@ -1,21 +1,41 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-export const Pagination = ({onPageChange}) => {
-    const [pages, setpages] = useState([1,2,3,4,5,6,7,8,9,10])
+export const Pagination = ({onPageChange, totalPages}) => {
+    const [pages, setpages] = useState([])
     const [activePage, setActivePage] = useState(1)
+    const THRESHOLD = 20
 
-    const handlePageChange = (page) => {
-        onPageChange(page)
-        setActivePage(page)
+    const setNewPagesList = (pageNo) => {
+        const itemLength = Math.min(totalPages, THRESHOLD)
+        let itemsOnLeft = Math.ceil(THRESHOLD / 2) - 1
+        let startingPage = Math.max(pageNo - itemsOnLeft, 1)
+
+        if(startingPage + itemLength > totalPages){
+            startingPage = totalPages - itemLength + 1
+        }
+        
+        const list = Array.from({length: Math.min(totalPages, THRESHOLD)}, (_, i) => startingPage + i)
+        setpages(list)
+    }
+
+    const handlePageChange = (pageNo) => {
+        onPageChange(pageNo)
+        setActivePage(pageNo)
+        setNewPagesList(pageNo)
 
     }
+
+    useEffect(() => {
+        const list = Array.from({length: Math.min(totalPages, THRESHOLD)}, (_, i) => i + 1);
+        setpages(list)
+    },[totalPages])
     
 
     return (
         <div className="pagination">
             <button onClick={()=>handlePageChange(activePage -1)} disabled={activePage == pages[0]} className="prev">Prev</button>
             {
-                pages.map((page) => (
+                pages.map(page => (
                     <button 
                         key={page} 
                         className={activePage === page ? 'active' : ''} 
