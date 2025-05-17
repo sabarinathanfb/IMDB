6e260692ee8e909d19fef9079f8555fd08e34559
sabarinathanfb <77018757+sabarinathanfb@users.noreply.github.com>
2025-05-17 09:38:48 +0530

    Watchlist sorting,filtering and Genere Selection

--- src/Pages/WatchList.jsx
+++ src/Pages/WatchList.jsx
@@ -1,20 +1,108 @@
+import { useEffect,useState } from "react"
+
+let genreids = {
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    14: "Fantasy",
+    36: "History",
+    27: "Horror",
+    10402: "Music",
+    9648: "Mystery",
+    10749: "Romance",
+    878: "Sci-Fi",
+    10770: "TV",
+    53: "Thriller",
+    10752: "War",
+    37: "Western",
+};
+
 export const WatchList = ({watchlist}) => {
+
+    const [list, setList] = useState([])
+
+
+    const handleSearch = (e) => {
+        const search = e.target.value.toLowerCase()
+        const filteredList = Object.values(watchlist).filter(movie => movie.title.toLowerCase().includes(search))
+        setList(filteredList)
+    }
+
+    const handleSorting = (order) => {
+        const sortedList = [...list].sort((a,b) => {
+            if(order === 'ASC'){
+                return a.popularity - b.popularity
+            }else{
+                return b.popularity - a.popularity
+            }
+        })
+        setList(sortedList)
+    }
+
+    const selectedGenres = () => {
+        let genreList = [];
+        Object.values(watchlist).forEach(movie => {
+            genreList = genreList.concat(movie.genre_ids);
+        });
+        return [ ...new Set(genreList)];
+    }
+
+    const handleGenreSelection = (genreId) => {
+        const newList = Object.values(watchlist).filter(movie => genreId ? movie.genre_ids.includes(genreId) : true);
+        setList(newList);
+    }
+
+
+    useEffect(() => {
+        setList(Object.values(watchlist))
+
+    },[watchlist])
+
     return(
         <div>
             <h1>Watch List</h1>
-            <table border={"1px"}>
-                <tr>
-                    <td>Id</td>
-                    <td>Title</td>
-                </tr>
-                {Object.keys(watchlist).map((key) => (
-                    <tr key={key}>
-                        <td>{key}</td>
-                        <td>{watchlist[key].title}</td>
-                    </tr>
-                ))}
 
-            </table>
+            <div className="watchlist">
+                <div className="left-section">
+                        <div className="genre-list">
+                            <div className="genre" onClick={() => handleGenreSelection('')}>All</div>
+                            {selectedGenres().map(genreId => (
+                                <div className="genre" onClick={() => handleGenreSelection(genreId)}>{genreids[genreId]}</div>
+                            ))}
+                        </div>
+                    </div>
+                <div className="right-section">
+                    <input type="text" placeholder="Search" onChange={handleSearch}/>
+                    <table border={"1px"}>
+                        <tr>
+                            <td>Id</td>
+                            <td>Poster</td>
+                            <td>Title</td>
+                            <td>Genre</td>
+                            <td>Popularity<span onClick={()=>handleSorting('DESC') }>^</span><span onClick={() =>handleSorting('ASC')}>V</span></td>
+                        </tr>
+                        {
+                            list.map(movie => (
+                                <tr>
+                                    <td>{movie.id}</td>
+                                    <td><img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} height='50px' /></td>
+                                    <td>{movie.title}</td>
+                                    <td>{movie.genre_ids.map(genreId => genreids[genreId]).join(', ')}</td>
+                                    <td>{movie.popularity}</td>
+                                </tr>
+                            ))
+                        }
+
+                    </table>
+                </div>
+            
+            </div>
+            
         </div>
     )
 }
